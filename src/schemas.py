81ea35e22c9b751d6c28a9a2e03a0a25b5996959
Ali Parandeh <Ali.Parandeh@outlook.com>
2024-03-07 17:58:22 +0000

    feat: update schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from typing import Annotated, Literal
+from uuid import uuid4
 
-from pydantic import (UUID4, BaseModel, Field, HttpUrl, IPvAnyAddress,
-                      PositiveInt)
+from pydantic import BaseModel, Field, HttpUrl, IPvAnyAddress, PositiveInt, computed_field
+
+from utils import count_tokens
 
 
 class ModelRequest(BaseModel):
@@ -11,7 +13,7 @@ class ModelRequest(BaseModel):
 
 class TextModelRequest(ModelRequest):
     model: Literal["tinyllama", "gemma2b"]
-    temperature: Annotated[float, Field(gte=0.0, lte=1.0, default=0.0)]
+    temperature: Annotated[float, Field(gt=0.0, lte=1.0, default=0.01)]
 
 
 class ImageModelRequest(ModelRequest):
@@ -21,16 +23,26 @@ class ImageModelRequest(ModelRequest):
 
 
 class ModelResponse(BaseModel):
-    request_id: UUID4
-    ip: IPvAnyAddress
-    content: Annotated[str | bytes, Field(min=0, max=5000)]
+    request_id: Annotated[str, Field(default_factory=lambda: uuid4().hex)]
+    ip: Annotated[str, IPvAnyAddress]
+    content: Annotated[str | bytes, Field(min=0, max=10000)]
     created_at: datetime = datetime.now()
 
 
 class TextModelResponse(ModelResponse):
-    tokens: Annotated[int, Field(gte=0)]
+    price: Annotated[float, Field(gte=0, default=0.01)]
+
+    @computed_field
+    @property
+    def tokens(self) -> int:
+        return count_tokens(self.content)
+
+    @computed_field
+    @property
+    def cost(self) -> float:
+        return self.price * self.tokens
 
 
 class ImageModelResponse(ModelResponse):
     size: tuple[int, int] = tuple[Field(gte=0), Field(gte=0)]
-    url: HttpUrl
+    url: HttpUrl | None = None
