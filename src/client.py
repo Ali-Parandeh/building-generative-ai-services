01cbf4952eccb3a1ce72163def46811dabf8ce0e
Ali Parandeh <Ali.Parandeh@outlook.com>
2024-03-18 16:27:36 +0000

    feat: update streamlit to render audio

--- src/client.py
+++ src/client.py
@@ -11,7 +11,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         content = message["content"]
         if isinstance(content, bytes):
-            st.image(content)
+            st.audio(content)
         else:
             st.markdown(content)
 
@@ -29,5 +29,11 @@ if prompt := st.chat_input("Write your prompt in this input field"):
         response = requests.get(f"http://localhost:8000/generate/image?prompt={prompt}").content
         st.text("Here is your generated image")
         st.image(response)
+    with st.chat_message("assistant"):
+        response = requests.get(f"http://localhost:8000/generate/audio?prompt={prompt}").content
+        st.text("Here is your generated audio")
+        st.audio(response)
 
-    st.session_state.messages.append({"role": "assistant", "content": "Here is your generated image"})
+    st.session_state.messages.append(
+        {"role": "assistant", "content": "Here is your generated audio"}
+    )
