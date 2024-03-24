a949bba2727febe3df9bb4d12a9b4b584c1cbece
Ali Parandeh <Ali.Parandeh@outlook.com>
2024-03-24 19:14:04 +0000

    feat: add video generation endpoint

--- src/main.py
+++ src/main.py
@@ -1,24 +1,43 @@
 import time
 from contextlib import asynccontextmanager
 from datetime import datetime
-from typing import Any, Callable, Generator
+from typing import Callable
 from uuid import uuid4
 
 import uvicorn
-from fastapi import BackgroundTasks, Body, FastAPI, HTTPException, Query, Request, Response, status
+from fastapi import (
+    BackgroundTasks,
+    Body,
+    FastAPI,
+    File,
+    HTTPException,
+    Query,
+    Request,
+    Response,
+    UploadFile,
+    status,
+)
 from fastapi.responses import RedirectResponse
+from PIL import Image
 from starlette.responses import StreamingResponse
 
 from models import (
     generate_audio,
     generate_image,
     generate_text,
+    generate_video,
     load_audio_model,
     load_image_model,
     load_text_model,
+    load_video_model,
 )
-from schemas import ImageModelRequest, TextModelRequest, TextModelResponse, VoicePresets
-from utils import audio_array_to_buffer, img_to_bytes
+from schemas import (
+    ImageModelRequest,
+    TextModelRequest,
+    TextModelResponse,
+    VoicePresets,
+)
+from utils import audio_array_to_buffer, export_to_video_buffer, img_to_bytes
 
 models = {}
 
@@ -70,7 +89,8 @@ def serve_text_to_text_controller(
 ) -> TextModelResponse:
     if body.model not in ["tinyllama", "gemma2b"]:
         raise HTTPException(
-            detail=f"Model {body.model} is not supported", status_code=status.HTTP_400_BAD_REQUEST
+            detail=f"Model {body.model} is not supported",
+            status_code=status.HTTP_400_BAD_REQUEST,
         )
     output = generate_text(models["text"], body.prompt, body.temperature)
     return TextModelResponse(content=output, ip=request.client.host)
@@ -88,7 +108,9 @@ def serve_text_to_image_model_controller(body: ImageModelRequest = Body(...)):
 
 
 @app.post("/generate/image/background")
-async def serve_image_model_background_controller(background_tasks: BackgroundTasks, prompt: str):
+async def serve_image_model_background_controller(
+    background_tasks: BackgroundTasks, prompt: str
+):
     background_tasks.add_task(process_image_generation, prompt)
     return {"message": "Task is being processed in the background"}
 
@@ -104,7 +126,25 @@ def serve_text_to_audio_model_controller(
 ):
     processor, model = load_audio_model()
     output, sample_rate = generate_audio(processor, model, prompt, preset)
-    return StreamingResponse(audio_array_to_buffer(output, sample_rate), media_type="audio/wav")
+    return StreamingResponse(
+        audio_array_to_buffer(output, sample_rate), media_type="audio/wav"
+    )
+
+
+@app.get(
+    "/generate/video",
+    responses={status.HTTP_200_OK: {"content": {"video/mpeg": {}}}},
+    response_class=StreamingResponse,
+)
+async def serve_text_to_audio_model_controller(
+    image: bytes = File(...), num_frames: int = Query(default=25)
+):
+    model = load_video_model()
+    image = Image.open(image)
+    frames = generate_video(model, image, num_frames)
+    return StreamingResponse(
+        export_to_video_buffer(frames), media_type="video/mp4"
+    )
 
 
 if __name__ == "__main__":
