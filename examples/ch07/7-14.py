2f4b7bf267abbccf0031f536b0a881800eb05bc8
Ali Parandeh <Ali.Parandeh@outlook.com>
2025-01-18 16:11:47 +0000

    feat: add Alembic dependency and initial migration script
    - Add alembic==1.14.0 to requirements.txt
    - Add isort==5.13.2 and Mako==1.3.8 to requirements.txt
    - Create an initial migration script (7-14.py) using Alembic

---
+++ examples/ch07/7-14.py
@@ -0,0 +1,23 @@
+# alembic/versions/2413cf32b712.py
+
+"""Initial Migration
+
+Revision ID: 2413cf32b712 Revises:
+Create Date: 2024-07-11 12:30:17.089406
+
+"""  # revision identifiers, used by Alembic.
+
+revision = "2413cf32b712"
+down_revision = None
+branch_labels = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
