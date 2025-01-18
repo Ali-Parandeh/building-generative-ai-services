1655f9cbb53c4a962c30a12f0612cb90b30d6dc5
Ali Parandeh <Ali.Parandeh@outlook.com>
2025-01-18 13:56:40 +0000

    refactor: add blank line between imports in 7-3.py
    feat: add new database session handling module 7-4.py

---
+++ examples/ch07/7-4.py
@@ -0,0 +1,24 @@
+# database.py
+
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from database import engine
+
+async_session = async_sessionmaker(
+    bind=engine, class_=AsyncSession, autocommit=False, autoflush=False
+)
+
+
+async def get_db_session():
+    try:
+        async with async_session() as session:
+            yield session
+    except:
+        await session.rollback()
+        raise
+    finally:
+        await session.close()
+
+
+DBSessionDep = Annotated[AsyncSession, Depends(get_db_session)]
