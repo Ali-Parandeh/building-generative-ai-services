b0d8aa6087d4f0023c244a4b86f5328135a2c403
Ali Parandeh <ali.parandeh@outlook.com>
2025-03-01 19:56:23 +0000

    feat(ch10): add example 10-6

---
+++ examples/ch10/10-6.py
@@ -0,0 +1,37 @@
+import uuid
+
+from qdrant_client import AsyncQdrantClient, models
+from qdrant_client.http.models import Distance, PointStruct
+
+
+class CacheClient:
+    def __init__(self):
+        self.db = AsyncQdrantClient(":memory:")
+        self.cache_collection_name = "cache"
+
+    async def create_cache_collection(self) -> None:
+        await self.db.create_collection(
+            collection_name=self.cache_collection_name,
+            vectors_config=models.VectorParams(
+                size=384, distance=Distance.EUCLID
+            ),
+        )
+
+    async def search(self, embedding) -> list[PointStruct]:
+        return await self.db.search(
+            collection_name=self.cache_collection_name,
+            query_vector=embedding,
+            limit=1,
+        )
+
+    async def insert(
+        self, query_vector: list[float], documents: list[str]
+    ) -> None:
+        point = PointStruct(
+            id=str(uuid.uuid4()),
+            vector=query_vector,
+            payload={"documents": documents},
+        )
+        await self.db.upload_points(
+            collection_name=self.cache_collection_name, points=[point]
+        )
