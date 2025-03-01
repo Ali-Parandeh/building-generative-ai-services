6d021ae9c2d92a27c9f59affb71bf30138e6c0cb
Ali Parandeh <ali.parandeh@outlook.com>
2025-03-01 20:39:33 +0000

    feat(ch10): add example 10-7

---
+++ examples/ch10/10-7.py
@@ -0,0 +1,29 @@
+from qdrant_client import AsyncQdrantClient, models
+from qdrant_client.http.models import Distance, ScoredPoint
+
+documents = [...]
+
+
+class DocumentStoreClient:
+    def __init__(self, host="localhost", port=6333):
+        self.db_client = AsyncQdrantClient(host=host, port=port)
+        self.collection_name = "docs"
+
+    async def init_db(self) -> None:
+        await self.db_client.create_collection(
+            collection_name=self.collection_name,
+            vectors_config=models.VectorParams(
+                size=384, distance=Distance.EUCLID
+            ),
+        )
+        await self.db_client.add(
+            documents=documents, collection_name=self.collection_name
+        )
+
+    async def query_database(self, query_text) -> list[ScoredPoint]:
+        results = await self.db_client.search(
+            query_vector=query_text,
+            limit=3,
+            collection_name=self.collection_name,
+        )
+        return results
