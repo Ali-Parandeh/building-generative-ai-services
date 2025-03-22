bd2691292f6dc61c060067ea68c5956c6a159678
Ali Parandeh <43654026+Ali-Parandeh@users.noreply.github.com>
2025-03-22 11:50:00 +0000

    fix client_audio.py

--- app/client_audio.py
+++ app/client_audio.py
@@ -23,9 +23,9 @@ if prompt := st.chat_input("Write your prompt in this input field"):
 
     response = requests.get(
         f"http://localhost:8000/generate/audio", params={"prompt": prompt}
-    ).content
+    )
     response.raise_for_status()
 
     with st.chat_message("assistant"):
         st.text("Here is your generated audio")
-        st.audio(response)
+        st.audio(response.content)
